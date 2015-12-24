8f9f452179b6fb92f7586a5d28d2d7b639c08277
Deepthi Kaligi <hardikdedhia@Krishhs-MacBook-Pro.local>
2015-12-24 11:36:36 +0530

    initial print method

--- gitproject/ViewController.swift
+++ gitproject/ViewController.swift
@@ -12,14 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+        func someMethodTotestGit() {
+            print("some mrthod")
+        }
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+  
 
 }
 
