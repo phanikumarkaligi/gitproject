889373d3058c06d309d7dc3215c394b904425e3f
Deepthi Kaligi <hardikdedhia@Krishhs-MacBook-Pro.local>
2015-12-24 13:13:59 +0530

    one

---
+++ gitproject/object.swift
@@ -0,0 +1,18 @@
+//
+//  object.swift
+//  gitproject
+//
+//  Created by Deepthi Kaligi on 24/12/2015.
+//  Copyright © 2015 TeamTreeHouse. All rights reserved.
+//
+
+import Foundation
+
+class object: NSObject {
+    
+    func one() {
+        
+    }
+    
+
+}
