f15d3ffbf1539e43cabe1c00abac4f740bf72fa6
Balu <hardikdedhia@krishhs-macbook-pro.local>
2015-12-24 11:18:30 +0530

    Initial Commit

---
+++ gitproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitproject
+//
+//  Created by Deepthi Kaligi on 24/12/2015.
+//  Copyright © 2015 TeamTreeHouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
